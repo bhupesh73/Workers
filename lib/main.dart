61f3828751194b012079246ea28aecd3b7fcd48d
rubi maharjan <rubymaharzn@gmail..com>
2024-06-03 21:44:42 +0545

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:loginuicolors/addtask.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:loginuicolors/dashboard.dart';
-import 'package:loginuicolors/login.dart';
+import 'package:loginuicolors/worker_login/page/login_page.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'login', 
-    routes: {
-      '/': (context) => Dashboard(), 
-      'login': (context) => MyLogin(),
-      'add_task': (context) => AddTask(), 
-    },
-  ));
+  runApp(MyApp(isLoggedIn: isLoggedIn));
+}
+
+class MyApp extends StatelessWidget {
+  final bool isLoggedIn;
+
+  const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vehicle',
+      home: isLoggedIn ? const Dashboard() : const MyLogin(),
+    );
+  }
 }
